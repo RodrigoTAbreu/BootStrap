55167fd9dadbea6d9405dc8cc8ac6c12a3ad18f4
RodrigoTAbreu <rodrigo.telles.abreu@gmail.com>
2020-10-03 05:25:03 -0300

    Desafios 1 e 2

---
+++ js/Desafio_Aritimetico02.js
@@ -0,0 +1,21 @@
+function calc(){
+    let vlr = document.getElementById('tvalor');
+    let c = 0;
+
+    for (let i = 1; i <= vlr.value; i++) {
+        if (i % 2 == 0) {
+          console.log(i);
+        }
+      
+      }
+
+
+/*
+    while (c<=vlr.value){
+        let resto = vlr.value % 2;
+            if (resto == 0 ){
+                console.log(`${c}`)
+        }
+    c = c*2;
+    }*/
+    }
