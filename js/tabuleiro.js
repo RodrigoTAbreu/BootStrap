9484b7f78881bec10018d7eb3a746b965d784119
RodrigoTAbreu <rodrigo.telles.abreu@gmail.com>
2020-10-10 15:21:19 -0300

    tabuleiro

---
+++ js/tabuleiro.js
@@ -0,0 +1,9 @@
+let a = '#'
+let resp = document.getElementById('tab')
+
+for(let i = 0; i <4;i++){
+    resp.innerHTML += ` ${a} `
+    resp.innerHTML +=` ` 
+}
+resp.innerHTML += `<p>${a}</p>`
+i = 0;
