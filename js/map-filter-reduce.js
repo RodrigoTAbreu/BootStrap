35ccd830393b1bfcf0ac529f395c1909a68bcabb
RodrigoTAbreu <rodrigo.telles.abreu@gmail.com>
2020-10-21 15:03:42 -0300

    MAp

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,12 @@
+const  pets = [
+    {name:'pull',
+    type: 'dog',
+    age: 10},
+    {name:'fifi',
+    type: 'cat',
+    age: 15},
+    {name:'frido',
+    type: 'fish',
+    age: 1},
+]
+console.log(pets)
